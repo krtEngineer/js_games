9fbb90b65f1ee2f6969114b5a0569b9c431ce555
krtEngineer <krt.engineer@gmail.com>
2023-12-27 16:53:58 +0530

    Adidng HTML for dice faces.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

---
+++ tenzi/diceFaces.js
@@ -0,0 +1,53 @@
+export const diceFace = {
+  1: {
+    html: `   <div class="dice face-1"><span class="dot"> </span></div>
+        </div>`,
+  },
+  2: {
+    html: `  <div class="dice face-2">
+            <span class="dot"> </span><span class="dot"> </span>
+          </div>`,
+  },
+  3: {
+    html: `<div class="dice face-3">
+            <span class="dot"> </span><span class="dot"> </span>
+            <span class="dot"> </span>
+          </div>`,
+  },
+  4: {
+    html: ` <div class="dice face-4">
+            <div class="dice-column">
+              <span class="dot"> </span><span class="dot"> </span>
+            </div>
+            <div class="dice-column">
+              <span class="dot"> </span><span class="dot"> </span>
+            </div>
+          </div>`,
+  },
+  5: {
+    html: ` <div class="dice face-5">
+            <div class="dice-column">
+              <span class="dot"> </span><span class="dot"> </span>
+            </div>
+            <div class="dice-column">
+              <span class="dot"> </span>
+            </div>
+            <div class="dice-column">
+              <span class="dot"> </span><span class="dot"> </span>
+            </div>
+          </div>`,
+  },
+  6: {
+    html: `          <div class="dice face-6">
+            <div class="dice-column">
+              <span class="dot"> </span><span class="dot"> </span>
+              <span class="dot"> </span>
+            </div>
+
+            <div class="dice-column">
+              <span class="dot"> </span><span class="dot"> </span>
+              <span class="dot"> </span>
+            </div>
+          </div>`,
+  },
+};
