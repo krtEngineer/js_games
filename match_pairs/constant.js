be20511f91d1b9050cb88ac88455baaf308df1b9
krtEngineer <krt.engineer@gmail.com>
2023-12-29 19:10:13 +0530

    Adding separate JS file for constants.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

---
+++ match_pairs/constant.js
@@ -0,0 +1,58 @@
+export const minEmojieCount = 0;
+export const maxEmojiecount = 16;
+/**
+ * Emojies list
+ * total emojis = 16
+ * short emoji list
+ * add more emojies by web scrapping or,
+ * to download huge emojies use // emojies list source link: https://gist.github.com/oliveratgithub/0bf11a9aff0d6da7b46f1490f86a71eb
+ */
+
+export const emojiies = [
+  {
+    html: "&#127815;",
+  },
+  {
+    html: "&#127816;",
+  },
+  { html: "&#127817;" },
+  {
+    html: "&#127818;",
+  },
+  {
+    html: "&#129372;",
+  },
+  {
+    html: "&#129362;",
+  },
+  {
+    html: "&#127798;",
+  },
+  {
+    html: "&#129365;",
+  },
+  {
+    html: "&#129361;",
+  },
+  {
+    html: "&#129367;",
+  },
+  {
+    html: "&#127871;",
+  },
+  {
+    html: "&#127846;",
+  },
+  {
+    html: "&#127850;",
+  },
+  {
+    html: "&#127856;",
+  },
+  {
+    html: "&#127852;",
+  },
+  {
+    html: "	&#9749;",
+  },
+];
