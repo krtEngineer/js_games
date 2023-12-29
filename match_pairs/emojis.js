0f17fab577afad12c35482654f0929dadbb7b1b6
krtEngineer <krt.engineer@gmail.com>
2023-12-29 19:11:25 +0530

    Moving extra methods to utility file.
    Signed-off-by: krtEngineer <krt.engineer@gmail.com>

--- match_pairs/emojis.js
+++ match_pairs/emojis.js
@@ -1,135 +1,49 @@
-import { getRandomIndex, getHtmlCode } from "./utils.js";
+import {
+  validateLength,
+  getUniqueRandomValues,
+  getDuplicateRandomValues,
+  getEmojieCode,
+} from "./utils.js";
+import { minEmojieCount, maxEmojiecount } from "./constant.js";
 
-const minEmojieCount = 0;
-const maxEmojiecount = 16;
-
+/**
+ * @description method to get list of random emojies
+ * @param {*} length length of emoji list
+ * @returns array
+ */
 export const getEmojieList = (length) => {
   try {
     validateLength(length);
     const randomEmojieList = getRandomEmojieList(length);
-    return getDoubledEmojieList(randomEmojieList, 2 * length);
+    return getDoubledEmojieList(randomEmojieList);
   } catch (e) {
     throw e;
   }
 };
 
-const validateLength = (length) => {
-  if (!isLengthValid(length)) {
-    throw new Error("Invalid length for emojies list.");
-  }
-};
-
-const isLengthValid = (length) => {
-  return length >= minEmojieCount && length <= maxEmojiecount;
-};
-
+/**
+ * @description method to get list of random emojies
+ * @param {*} length length of random emojies list
+ * @returns array
+ */
 const getRandomEmojieList = (length) => {
   const emojieList = [];
-  const uniqueRandomIndices = getUniqueRandomIndices(
-    length,
-    minEmojieCount,
-    maxEmojiecount
-  );
-  uniqueRandomIndices.map((index) => {
+  getUniqueRandomValues(length, minEmojieCount, maxEmojiecount).map((index) => {
     const code = getEmojieCode(index);
     emojieList.push(code);
   });
   return emojieList;
 };
 
-const getEmojieCode = (index) => {
-  return getHtmlCode(emojiies[index].html);
-};
-
-const getUniqueRandomIndices = (length, min, max) => {
-  let randomIndices = [];
-  while (!areAllIndicesUnique(randomIndices, length)) {
-    const randomIndex = getRandomIndex(min, max);
-    randomIndices.push(randomIndex);
-  }
-  return getUniqueIndices(randomIndices);
-};
-
-const areAllIndicesUnique = (indices, length) => {
-  const indexSet = new Set(indices);
-  return indexSet.size === length;
-};
-
-const getUniqueIndices = (indices) => {
-  const indexSet = new Set(indices);
-  return [...indexSet];
-};
-
-const getDuplicateIndices = (length) => {
-  const halvedLength = length / 2;
-  const duplicateIndices = [];
-  getUniqueRandomIndices(length, minEmojieCount, maxEmojiecount).map(
-    (index) => {
-      duplicateIndices.push(index % halvedLength);
-    }
-  );
-  return duplicateIndices;
-};
-console.log(getDuplicateIndices(16));
-
-const getDoubledEmojieList = (randomEmojieList, length) => {
+/**
+ * @description method to duplicate emojies in random order
+ * @param {*} randomEmojieList list of random emojies
+ * @returns array
+ */
+const getDoubledEmojieList = (randomEmojieList) => {
   let emojieList = [];
-  getDuplicateIndices(length).map((index) => {
+  getDuplicateRandomValues(randomEmojieList.length * 2).map((index) => {
     emojieList.push(randomEmojieList[index]);
   });
   return emojieList;
 };
-
-// total emojis = 16
-// short emoji list
-// add more emojies by web scrapping
-// to download huge emojies use // emojies list source link: https://gist.github.com/oliveratgithub/0bf11a9aff0d6da7b46f1490f86a71eb
-
-const emojiies = [
-  {
-    html: "&#127815;",
-  },
-  {
-    html: "&#127816;",
-  },
-  { html: "&#127817;" },
-  {
-    html: "&#127818;",
-  },
-  {
-    html: "&#129372;",
-  },
-  {
-    html: "&#129362;",
-  },
-  {
-    html: "&#127798;",
-  },
-  {
-    html: "&#129365;",
-  },
-  {
-    html: "&#129361;",
-  },
-  {
-    html: "&#129367;",
-  },
-  {
-    html: "&#127871;",
-  },
-  {
-    html: "&#127846;",
-  },
-  {
-    html: "&#127850;",
-  },
-  {
-    html: "&#127856;",
-  },
-  {
-    html: "&#127852;",
-  },
-  {
-    html: "	&#9749;",
-  },
-];
